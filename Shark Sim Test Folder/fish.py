38766a77d19459ca62d26350f44f551d4178a4d9
RichardHohn <richard_hohn@aw.org>
2023-01-10 15:38:50 -0800

    Completed

---
+++ Shark Sim Test Folder/fish.py
@@ -0,0 +1,24 @@
+from graphics import *
+from shark import *
+
+class fishes:
+    
+    def __init__(self, pos, win):
+
+        #self.pos = Point(470, 110)
+    
+        fish_1 = Image(Point(470, 110), "fish.png")
+        fish_1.draw(win)
+
+        fish_2 = Image(Point(450, 110), "fish.png")
+        fish_2.draw(win)
+
+        fish_3 = Image(Point(430, 110), "fish.png")
+        fish_3.draw(win)
+
+
+    #def fish1:
+    #def fish2:
+    #def fish3:   
+        
+        
